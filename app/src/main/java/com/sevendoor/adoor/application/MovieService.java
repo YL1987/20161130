5a9596d6e8264bd53ee9ebda3462750d17f428fa
yangyouqiao <648438037@qq.com>
2016-11-30 18:10:07 +0800

    sAS

---
+++ app/src/main/java/com/sevendoor/adoor/application/MovieService.java
@@ -0,0 +1,12 @@
+package com.sevendoor.adoor.application;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by yangyouqiao on 2016/11/23.
+ */
+public interface MovieService {
+//    @GET("top250")
+//    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+}
